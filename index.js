028830101156f0cb8ee9c41340c8ff86ace6754b
Lee Allen <lee@Lees-iMac.local>
2022-01-27 21:21:26 +0000

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 
 const required = (value) => {
-    return 'This field is required'
+    if (!value|| value === !false){
+       return 'This field is required' 
+    }
+    
 }
+
+const validateNumber = (value) => {
+    if(value && isNaN(value)) {
+        return "Must be a number";
+    }
+}
+
+const zeroToOneHundred = (value) => {
+    if(value && isNaN(value) || value && value < 0 || value > 100) {
+        return "Must be a number between 0 and 100"
+    }
+}
+
+const maxLength20 = (value) => {
+    if(value && isNaN(value) && value.length > 19) {
+        "Must be less than 20 characters long"
+    }
+} 
